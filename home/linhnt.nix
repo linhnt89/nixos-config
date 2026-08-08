f837d7d4e40e7d10f0a7e5b4732607fd51aaa769
Linh Nguyen <linhtramnguyen@gmail.com>
2026-08-08 23:42:42 +0700

    Add Polkit agent and notifications

--- home/linhnt.nix
+++ home/linhnt.nix
@@ -1,18 +1,18 @@
-{ ... }:
+{ pkgs, ... }:
 
 {
   home.username = "linhnt";
   home.homeDirectory = "/home/linhnt";
 
   programs.git = {
-	enable = true;
+    enable = true;
 	
-	settings = {
-		user = {
-			name = "Linh Nguyen";
-			email = "linhtramnguyen@gmail.com";
-		};
-	};
+    settings = {
+      user = {
+        name = "Linh Nguyen";
+	email = "linhtramnguyen@gmail.com";
+      };
+    };
   };
 
   programs.zsh.enable = true;
@@ -25,6 +25,31 @@
 
   # Bootstrap application launcher
   programs.fuzzel.enable = true;
+  
+  # Authentication
+  services.hyprpolkitagent.enable = true;
+  # Notification
+  services.mako.enable = true;
+  systemd.user.services.mako = {
+    Unit = {
+      Description = "Mako notification daemon";
+      After = [ "graphical-session.target" ];
+      PartOf = [ "graphical-session.target" ];
+    };
+
+    Service = {
+      ExecStart = "${pkgs.mako}/bin/mako";
+      Restart = "on-failure";
+    };
+
+    Install = {
+      WantedBy = [ "graphical-session.target" ];
+    };
+  };
+
+  home.packages = [
+    pkgs.libnotify
+  ];
 
   # Keep Hyprland's native Lua config as a normal file for now.
   xdg.configFile."hypr/hyprland.lua".source =
